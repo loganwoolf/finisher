185cae02f93aa4e5f52195e534f10be32620e458
Logan Woolf <loganwoolf@gmail.com>
2021-10-28 15:28:49 -0600

    create project and task modules

---
+++ src/modules/createProject.js
@@ -0,0 +1,35 @@
+
+const projects = (() => {
+
+	const projectList = []
+
+	const projectFactory = ( projectName ) => {
+		const timestamp = new Date()
+		
+		let name = projectName
+		const id = timestamp.getTime()
+		
+		return {
+			get name() {
+				return name
+			},
+			set newName(newName) {
+				name = newName
+			},
+			get id() {
+				return id
+			}
+		}
+	}
+	
+	const addProject = ( name ) => {
+		const newProject = projectFactory(name)
+		projectList.push(newProject)
+	}
+	
+	addProject('Main')
+
+	return { projectList }
+})() 
+
+export { projects }
---
+++ src/modules/createTask.js
@@ -0,0 +1,85 @@
+
+const tasks = (() => {
+	
+	const taskList = []
+
+	const taskFactory = (taskTitle, projectId) => {
+		const timestamp = new Date()
+
+		let title = taskTitle
+		const id = timestamp.getTime()
+		let parentProject = ''// projects.projectList[0] // interface needed
+		let description = ''
+		let dueDate = ''
+		let priority = false
+		let notes = ''
+		let checklist = []
+
+
+		return {
+			get title() {
+				return title
+			},
+			set newTitle(newTitle) {
+				title = newTitle
+			},
+
+			get id() {
+				return id
+			},
+
+			get parentProject() {
+				return parentProject
+			},
+			set newParentProject(newProject) {
+				parentProject = newProject
+			},
+
+			get description() {
+				return description
+			},
+			set newDescription(newDescription) {
+				description = newDescription
+			},
+
+			get dueDate() {
+				return dueDate
+			},
+			set newDueDate(newDueDate) {
+				dueDate = newDueDate
+			},
+
+			get priority() {
+				return priority
+			},
+			set newPriority(newPriority) {
+				priority = newPriority
+			},
+
+			get notes() {
+				return notes
+			},
+			set newNotes(newNotes) {
+				notes = newNotes
+			},
+
+			get checklist() {
+				return checklist
+			},
+			set newChecklist(newChecklist) {
+				checklist = newChecklist
+			},
+		}
+	}
+
+	const addTask = ( title ) => {
+		const newTask = taskFactory(title)
+		taskList.push(newTask)
+	}
+
+	addTask('First task') // remove for prod
+
+	return { taskList }
+})()
+
+export { tasks }
