85c452227fdcce0792af0381c61d66dbfe4fb539
Logan Woolf <loganwoolf@gmail.com>
2021-12-23 07:53:46 -0700

    import modules into index.js, create listener fn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import './style.css'
 import { app } from './layouts/app'
+import { tasks } from './modules/createTask'
+import { projects } from './modules/createProject'
+
+const addListeners = (() => {
+	
+})()
 
 function moduleRender() {
 	const jsTarget = document.createElement('div')
 	jsTarget.classList.add('js-target')
 
 	// modules to load below
-	jsTarget.appendChild(app())
-	
+	jsTarget.appendChild(app.layoutElement)
 
 	return jsTarget
 }
