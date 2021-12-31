ad4e51776361dffcbe10af540343a007bc8dd130
Logan Woolf <loganwoolf@gmail.com>
2021-12-31 01:33:06 -0700

    massive refactor into separate layouts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import './style.css'
 import { app } from './layouts/app'
 
-
-function moduleRender() {
-	const jsTarget = document.createElement('div')
-	jsTarget.classList.add('js-target')
-
-	// modules to load below
-	jsTarget.appendChild(app.layoutElement)
-	
-
-	return jsTarget
-}
-
-document.body.appendChild(moduleRender())
-app.addProjectTabListener()
+document.body.appendChild(app.layoutElement)
---
+++ src/layouts/projects.js
@@ -0,0 +1,39 @@
+import { projects } from "../modules/createProject"
+
+// conditionally render default projects
+if (projects.projectList.length === 0) {
+	projects.addProject('Main')
+	projects.addProject('Personal')
+}
+
+const projectLayout = (() => {
+
+	function renderProjectTabs () {
+		projects.projectList.forEach( ( project, index ) => {
+			const projectTab = document.createElement('button')
+			projectTab.classList.add('project-tab')
+			projectTab.dataset.project = project.name
+			index === 0 ? projectTab.classList.add('active-project-tab') : undefined
+
+			projectTab.textContent = project.name
+
+			tabs.appendChild(projectTab)
+		})
+		// create add new project button
+		const newProjectButton = document.createElement('button')
+		newProjectButton.classList.add('project-tab')
+		newProjectButton.classList.add('new-project')
+		newProjectButton.textContent = '➕'
+
+		tabs.appendChild(newProjectButton)
+	}
+
+	// render project tabs
+	const tabs = document.createElement('div')
+	tabs.classList.add('project-tabs')
+	renderProjectTabs()
+
+  return { tabs }
+})()
+
+export { projectLayout }
