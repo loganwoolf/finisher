dd85d782c9f96d8aff7dee61cfb837e3872cb40e
Logan Woolf <loganwoolf@gmail.com>
2022-01-02 02:58:45 -0700

    add button functionality to create new projects

--- src/layouts/app.js
+++ src/layouts/app.js
@@ -1,36 +1,88 @@
 import { projects } from "../modules/createProject";
 import { projectLayout } from "./projects";
 import { taskLayout } from "./tasks";
+import { utility as u } from "../modules/utilityFunctions";
 
 
 const app = (() => {
 
   function addProjectTabListener () {
-    projectLayout.tabs.addEventListener('click', switchCurrentProject)
+    projectLayout.tabs.addEventListener('click', determineButtonAction)
   }
 
-	function switchCurrentProject (e) {
+	function switchActiveProject (e) {
 		// check which project button was clicked
 		let elementNumber = [...e.target.parentNode.children].indexOf(e.target)
+		// remove active project tab class from previous tab
+		const previousProjectTab = document.querySelector('.active-project-tab')
+		previousProjectTab.classList.remove('active-project-tab')
+
+		// update state to new project
+		taskLayout.state.updateCurrentProject(elementNumber)
+
+		// remove visible tasks
+		const taskElement = document.querySelector('.task-list')
+		taskElement.replaceChildren()
+
+		// apply active project tab style
+		e.target.classList.add('active-project-tab')
+
+		// render tasks from new current project
+		taskLayout.renderCurrentTasks(taskLayout.state.currentProject)
+
+	}
+
+	function createNewProject (e) {
+		const newProjectButton = e.target
+
+		// remove active project tab class from previous tab
+		const previousProjectTab = document.querySelector('.active-project-tab')
+		previousProjectTab.classList.remove('active-project-tab')
+
+		// apply active project tab style
+		e.target.classList.add('active-project-tab')
+
+		// clear innerText of button
+		newProjectButton.innerText = null
+
+		// create form on button to take input of new project name
+		const field = document.createElement('input')
+		field.type = 'text'
+		field.addEventListener('input', u.growInputField)
+		
+		const form = document.createElement('form')
+		form.appendChild(field)
+		newProjectButton.appendChild(form)
+		field.focus()
+
+		//
+		form.addEventListener('submit', (e) => {
+			e.preventDefault()
+			// check no other project with that name exists
+			//
+			//
+
+			// create the project
+			const newProjectName = e.target.firstChild.value
+			projects.addProject(newProjectName)
+
+			// render project tabs again
+			projectLayout.renderProjectTabs()
+
+			//select the newly made project
+			document.querySelector(`[data-project=${newProjectName}`).click()
+			//begin creating first task
+			document.querySelector('.create-task').click()
+		})
+
+	}
+
+	function determineButtonAction (e) {
 		// check that only a project button was clicked
-		if (e.target.localName === 'button' && ![...e.target.classList].includes('new-project')) {
-				// remove active project tab class from previous tab
-				const previousProjectTab = document.querySelector('.active-project-tab')
-				previousProjectTab.classList.remove('active-project-tab')
-				
-				// update state to new project
-				taskLayout.state.updateCurrentProject(elementNumber)
-				
-				// remove visible tasks
-				const taskElement = document.querySelector('.task-list')
-				taskElement.replaceChildren()
-				
-				// apply active project tab style
-				e.target.classList.add('active-project-tab')
-				
-				// render tasks from new current project
-				taskLayout.renderCurrentTasks(taskLayout.state.currentProject)
-				
+		if (e.target.dataset.project) {
+			switchActiveProject(e)
+		} else if ([...e.target.classList].includes('new-project')) {
+			createNewProject(e)
 		}
 	}
 
