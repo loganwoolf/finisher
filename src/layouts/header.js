2db3cd53aa3bf77f7323db676ae27e2bcdf170ce
Logan Woolf <loganwoolf@gmail.com>
2022-01-08 10:26:30 -0700

    Create header element
    Not yet mounted to app
    Not yet styled

---
+++ src/layouts/header.js
@@ -0,0 +1,24 @@
+
+
+const header = ( () => {
+  const APPTITLE = 'FINISHER'
+  const SUBTITLE = 'Getting your tasks DONE!'
+
+  const element = document.createElement('header')
+
+  const appTitle = document.createElement('h1')
+  appTitle.innerText = APPTITLE
+
+  const subTitle = document.createElement('h2')
+  subTitle.innerText = SUBTITLE
+
+
+  element.appendChild(appTitle)
+  element.append(subTitle)
+
+
+  return { element }
+
+} ) ()
+
+export { header }
