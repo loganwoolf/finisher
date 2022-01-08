ea1e2a3e15709c26dbd55faac3b8f75d8e3cc788
Logan Woolf <loganwoolf@gmail.com>
2022-01-08 15:00:57 -0700

    Fix footer element heirarchy
    Improper nesting of 'a' tag inside 'p' tag. Now elements are adjacent

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -2,20 +2,23 @@
 
 const footer = ( () => {
 
-  const ATTRIBUTION = 'Coded in 🍁 by '
+  const ATTRIBUTION = 'Built in 🍁 by '
   const LINKTEXT = 'Logan Woolf'
   const GITHUBLINK = 'https://www.github.com/loganwoolf'
 
   const element = document.createElement('footer')
 
-  const githubLink = document.createElement('a')
-  githubLink.href = GITHUBLINK
-  githubLink.innerText = LINKTEXT
   const attribution = document.createElement('p')
   attribution.innerHTML = ATTRIBUTION
-  attribution.appendChild(githubLink)
+
+  const githubLink = document.createElement('a')
+  githubLink.classList.add('zoom')
+  githubLink.href = GITHUBLINK
+  githubLink.innerText = LINKTEXT
+
 
   element.appendChild(attribution)
+  element.appendChild(githubLink)
   
   return { element }
 
